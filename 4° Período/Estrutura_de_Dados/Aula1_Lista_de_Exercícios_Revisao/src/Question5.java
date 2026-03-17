be7299f3d4facc4d79bb66788db68dd76e2ee1e2
DanielFMorais <daniel.d.fm0512@gmail.com>
2026-03-17 15:14:16 -0300

    Finalização da atividade

--- 4° Período/Estrutura_de_Dados/Aula1_Lista_de_Exercícios_Revisao/src/Question5.java
+++ 4° Período/Estrutura_de_Dados/Aula1_Lista_de_Exercícios_Revisao/src/Question5.java
@@ -5,14 +5,18 @@ public class Question5 {
         Random r = new Random();
         int vetorAle[] = new int[100];
         int vetorDec[] = new int[100];
-        
+        int trocasAle=0;
+        int trocasDec=0;
+        //Preenchimento e visualização de vetor
         for (int i=0; i<vetorAle.length;i++){
             vetorAle[i] = r.nextInt(100);
         }
         for (int i = 0; i < vetorDec.length; i++){
         vetorDec[i] = 100 - i;
         }   
+    
         
+        //Preenchimento e visualização de vetor
         for(int i:vetorAle){
             System.out.print(i + " ");
         }
@@ -22,6 +26,31 @@ public class Question5 {
             System.out.print(i + " ");
         }
         
+        //Ordenação do aleatório com contagem
+        for(int i=0; i<vetorAle.length; i++){
+            for(int j=0; j<vetorAle.length - 1 - i; j++ ){
+                if(vetorAle[j] > vetorAle[j+1]){
+                    int temp = vetorAle[j];
+                    vetorAle[j] =vetorAle[j +1];
+                    vetorAle[j+1] = temp;
+                    trocasAle++;;//Realiza a contagem
+                }
+            }
+        }
+
+        //ordenação do 100 -1
+        for(int i=0; i<vetorDec.length; i++){
+            for(int j=0; j<vetorDec.length - 1 - i; j++ ){
+                if(vetorDec[j] > vetorDec[j+1]){
+                    int temp = vetorDec[j];
+                    vetorDec[j] =vetorDec[j +1];
+                    vetorDec[j+1] = temp;
+                    trocasDec++;//Realiza a contagem
+                }
+            }
+        }
+
+        System.out.printf("\n\n\nTrocas do vetor ALEATÓRIO: %d\nTroca do vetor de 100 á 1: %d",trocasAle,trocasDec);
         
 
 
